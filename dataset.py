a096a166cbb4da1ce7d3358473fefec84933a58c
anant15 <ai.with.anant@gmail.com>
2020-04-20 09:53:06 +0530

    First trial changes

--- dataset.py
+++ dataset.py
@@ -2,14 +2,21 @@ import os
 import io
 import random
 from PIL import Image
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class Dataset(object):
     def __init__(self, images_dir, patch_size=48, jpeg_quality=40, transforms=None):
         self.images = os.walk(images_dir).__next__()[2]
         self.images_path = []
         for img_file in self.images:
-            self.images_path.append(os.path.join(images_dir, img_file))
-
+            if img_file.endswith((".ppm")):
+                try:
+                    #print(os.path.join(images_dir, img_file))
+                    label = Image.open(os.path.join(images_dir, img_file))
+                    self.images_path.append(os.path.join(images_dir, img_file))
+                except:
+                    print(f"Image {os.path.join(images_dir, img_file)} didn't get loaded")
         self.patch_size = patch_size
         self.jpeg_quality = jpeg_quality
         self.transforms = transforms
@@ -17,7 +24,6 @@ class Dataset(object):
 
     def __getitem__(self, idx):
         label = Image.open(self.images_path[idx]).convert('RGB')
-
         label = label.rotate(self.random_rotate[random.randrange(0,4)])
 
         # randomly crop patch from training set
@@ -28,14 +34,14 @@ class Dataset(object):
 
         # additive jpeg noise
         buffer = io.BytesIO()
-        label.save(buffer, format='jpeg', quality=random.range(self.jpeg_quality+1))
+        label.save(buffer, format='jpeg', quality= random.randrange(self.jpeg_quality+1))
 
         input = Image.open(buffer).convert('RGB')
 
         if self.transforms is not None:
             input = self.transforms(input)
             label = self.transforms(label)
-
+        #print("Image transformed")
         return input, label
 
     def __len__(self):
--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import torch
 import torch.backends.cudnn as cudnn
 from torchvision import transforms
 import PIL.Image as pil_image
+import glob
 
 from edar import EDAR
 
@@ -14,14 +15,11 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights_path', type=str, required=True)
-    parser.add_argument('--image_path', type=str, required=True)
+    parser.add_argument('--image_path', type=str, required=False)
     parser.add_argument('--outputs_dir', type=str, required=True)
     parser.add_argument('--jpeg_quality', type=int, default=40)
-    opt = parser.parse_args()
-
-    if not os.path.exists(opt.outputs_dir):
-        os.makedirs(opt.outputs_dir)
-
+    parser.add_argument('--input_dir', type=str, required=False)
+    opt, unknown = parser.parse_known_args()
     model = EDAR()
 
     state_dict = model.state_dict()
@@ -32,23 +30,40 @@ if __name__ == '__main__':
             raise KeyError(n)
 
     model = model.to(device)
+    print(device)
     model.eval()
+    
+    if opt.input_dir:
+        filenames = [os.path.join(opt.input_dir, file) for file in os.listdir(opt.input_dir) if file.endswith(("ppm", "jpeg", "png", "jpg"))]
+        print(filenames)
+    else:
+        filenames = opt.image_path
+        
+    if not os.path.exists(opt.outputs_dir):
+        os.makedirs(opt.outputs_dir)
+    
+    for filename in filenames:
+        print("file is", filename)
+        input = pil_image.open(filename).convert('RGB')
+        print("Input size:", input.size)
 
-    filename = os.path.basename(opt.image_path).split('.')[0]
+        #buffer = io.BytesIO()
+        #input.save(buffer, format='jpeg', quality=opt.jpeg_quality)
+        #input = pil_image.open(buffer)
+        #input.save(os.path.join(opt.outputs_dir, '{}_jpeg_q{}.png'.format(filename, opt.jpeg_quality)))
 
-    input = pil_image.open(opt.image_path).convert('RGB')
+        input = transforms.ToTensor()(input).unsqueeze(0).to(device)
+        output_path = os.path.join(opt.outputs_dir, '{}-{}.jpeg'.format(filename.split("/")[-1].split(".")[0], "edar"))
+        
+        if not os.path.exists(output_path):
+            with torch.no_grad():
+                pred = model(input)[-1]
 
-    buffer = io.BytesIO()
-    input.save(buffer, format='jpeg', quality=opt.jpeg_quality)
-    input = pil_image.open(buffer)
-    input.save(os.path.join(opt.outputs_dir, '{}_jpeg_q{}.png'.format(filename, opt.jpeg_quality)))
-
-    input = transforms.ToTensor()(input).unsqueeze(0).to(device)
-
-    with torch.no_grad():
-        pred = model(input)[-1]
-
-    pred = pred.mul_(255.0).clamp_(0.0, 255.0).squeeze(0).permute(1, 2, 0).byte().cpu().numpy()
-    output = pil_image.fromarray(pred, mode='RGB')
-    output.save(os.path.join(opt.outputs_dir, '{}_{}.png'.format(filename, "EDAR")))
+            pred = pred.mul_(255.0).clamp_(0.0, 255.0).squeeze(0).permute(1, 2, 0).byte().cpu().numpy()
+            output = pil_image.fromarray(pred, mode='RGB')
+            print("Output size", output.size)
+            print("Output dir is", opt.outputs_dir)
+            output.save(output_path)
+            #print(os.path.join(opt.outputs_dir, '{}_{}.png'.format(filename, "EDAR")))
+            #print("Output saved")
 
